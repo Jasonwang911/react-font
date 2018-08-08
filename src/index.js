58fd52cdbb6f8a4fb192729cea6b9451d4c5feb4
王深 <wangshen1@yingu.com>
2018-08-08 19:38:15 +0800

    添加alise、打包path、NODE_ENV

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+alert(process.env.REACT_APP_SECRET_API)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/view/text.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Text extends Component {
+  render() {
+    return (
+      <div>
+        我是测试的组件
+      </div>
+    );
+  }
+}
+
+export default Text;
